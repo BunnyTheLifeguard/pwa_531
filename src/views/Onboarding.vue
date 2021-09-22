87ec399461ae85c43c26cdc86997b5e8ecc96d6a
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2021-09-22 20:54:02 +0200

    Push onboarding

--- src/views/Onboarding.vue
+++ src/views/Onboarding.vue
@@ -1,17 +1,62 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import SwiperCore, { Pagination } from 'swiper'
+import Toggle from '@vueform/toggle'
+import localforage from 'localforage'
 import BaseButton from '../components/BaseButton.vue'
+import Toast from '../components/Toast.vue'
+
+const router = useRouter()
 
 SwiperCore.use([Pagination])
+
+const unit = ref(true)
+const squatMax = ref(0)
+const deadliftMax = ref(0)
+const benchpressMax = ref(0)
+const overheadMax = ref(0)
+const trainingDays = ref([])
+
+const notification = ref('')
+
+const storeValues = async () => {
+  if (
+    squatMax.value === 0 ||
+    deadliftMax.value === 0 ||
+    benchpressMax.value === 0 ||
+    overheadMax.value === 0 ||
+    trainingDays.value.length === 0
+  ) {
+    notification.value = 'Please enter a value for every exercise.'
+    setTimeout(() => {
+      notification.value = ''
+    }, 3000)
+  } else {
+    await localforage.setItem('data', {
+      week: 1,
+      day: 1,
+      date: new Date(),
+      trainingDays: trainingDays._rawValue,
+      unit: unit.value,
+      squat: +squatMax.value,
+      deadlift: +deadliftMax.value,
+      benchpress: +benchpressMax.value,
+      overheadpress: +overheadMax.value
+    })
+    console.log('push home')
+    await router.push({ name: 'home' })
+  }
+}
 </script>
 
 <template>
   <Swiper
     :pagination="{ dynamicBullets: true }"
-    class="h-screen w-screen bg-white dark:bg-dark-100"
+    class="h-screen w-screen bg-white dark:bg-dark-200"
   >
     <SwiperSlide
       class="
@@ -130,17 +175,61 @@ SwiperCore.use([Pagination])
         dark:text-white
       "
     >
-      <span class="mb-10 font-bold uppercase tracking-wide">Start</span>
+      <Toast v-if="notification">{{ notification }}</Toast>
+      <!-- <span class="mb-10 font-bold uppercase tracking-wide">Start</span> -->
       <span class="mx-10 mb-3 text-center"
         >To start, enter your 1 Rep Max for every exercise.</span
       >
       <span class="mx-10 mb-5 text-center"
         >If you don't know your 1RM, use the calculators.</span
       >
-      <form action="" class="flex flex-col mt-5">
+      <form class="flex flex-col justify-items-center mt-5">
+        <div class="mb-5 flex justify-center">
+          <Toggle
+            v-model="unit"
+            on-label="KG"
+            off-label="LBS"
+            :classes="{
+              container: 'inline-block',
+              toggle:
+                'flex w-16 h-5 relative cursor-pointer transition items-center box-content border-2 text-xs leading-none',
+              toggleOn: 'bg-blue-500 border-blue-600 justify-start text-white',
+              toggleOff:
+                'bg-gray-200 border-gray-200 justify-end text-gray-700',
+              toggleOnDisabled:
+                'bg-gray-300 border-gray-300 justify-start text-gray-400 cursor-not-allowed',
+              toggleOffDisabled:
+                'bg-gray-200 border-gray-200 justify-end text-gray-400 cursor-not-allowed',
+              handle:
+                'inline-block bg-white w-5 h-5 top-0 absolute transition-all',
+              handleOn: 'left-full transform -translate-x-full',
+              handleOff: 'left-0',
+              handleOnDisabled:
+                'bg-gray-100 left-full transform -translate-x-full',
+              handleOffDisabled: 'bg-gray-100 left-0',
+              label: 'text-center w-8 border-box whitespace-nowrap select-none'
+            }"
+          />
+        </div>
+        <!-- Squats -->
         <label for="squat">1RM Squats:</label>
         <div class="flex items-center">
-          <input type="number" id="squat" class="my-2 py-2 mr-5" />
+          <input
+            v-model="squatMax"
+            type="number"
+            id="squat"
+            class="
+              my-2
+              py-2
+              mr-5
+              bg-transparent
+              dark:bg-dark-50
+              text-center
+              border border-dark-50
+              dark:border-transparent
+              shadow-inner
+            "
+          />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -158,10 +247,25 @@ SwiperCore.use([Pagination])
             ></path>
           </svg>
         </div>
-
+        <!-- Deadlifts -->
         <label for="deadlift">1RM Deadlifts</label>
         <div class="flex items-center">
-          <input type="number" id="deadlift" class="my-2 py-2 mr-5" />
+          <input
+            v-model="deadliftMax"
+            type="number"
+            id="deadlift"
+            class="
+              my-2
+              py-2
+              mr-5
+              bg-transparent
+              dark:bg-dark-50
+              text-center
+              border border-dark-50
+              dark:border-transparent
+              shadow-inner
+            "
+          />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -179,9 +283,25 @@ SwiperCore.use([Pagination])
             ></path>
           </svg>
         </div>
+        <!-- Benchpress -->
         <label for="benchpress">1RM Benchpress</label>
         <div class="flex items-center">
-          <input type="number" id="benchpress" class="my-2 py-2 mr-5" />
+          <input
+            v-model="benchpressMax"
+            type="number"
+            id="benchpress"
+            class="
+              my-2
+              py-2
+              mr-5
+              bg-transparent
+              dark:bg-dark-50
+              text-center
+              border border-dark-50
+              dark:border-transparent
+              shadow-inner
+            "
+          />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -199,9 +319,25 @@ SwiperCore.use([Pagination])
             ></path>
           </svg>
         </div>
+        <!-- OH Press -->
         <label for="overheadpress">1RM Overheadpress</label>
         <div class="flex items-center">
-          <input type="number" id="overheadpress" class="my-2 py-2 mr-5" />
+          <input
+            v-model="overheadMax"
+            type="number"
+            id="overheadpress"
+            class="
+              my-2
+              py-2
+              mr-5
+              bg-transparent
+              dark:bg-dark-50
+              text-center
+              border border-dark-50
+              dark:border-transparent
+              shadow-inner
+            "
+          />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -219,7 +355,81 @@ SwiperCore.use([Pagination])
             ></path>
           </svg>
         </div>
-        <BaseButton class="mt-3 p-3 bg-blue-600">Start</BaseButton>
+        <!-- Training days -->
+        <span class="my-3"
+          >Training days <i class="font-normal">(3-4 recommended)</i>:</span
+        >
+        <div class="flex items-center mb-3">
+          <div class="flex flex-col items-center m-3">
+            <label for="mon" class="mb-1">M</label>
+            <input
+              type="checkbox"
+              value="mon"
+              id="mon"
+              v-model="trainingDays"
+            />
+          </div>
+          <div class="flex flex-col items-center m-3">
+            <label for="tue" class="mb-1">T</label>
+            <input
+              type="checkbox"
+              value="tue"
+              id="tue"
+              v-model="trainingDays"
+            />
+          </div>
+          <div class="flex flex-col items-center m-3">
+            <label for="wed" class="mb-1">W</label>
+            <input
+              type="checkbox"
+              value="wed"
+              id="wed"
+              v-model="trainingDays"
+            />
+          </div>
+          <div class="flex flex-col items-center m-3">
+            <label for="thu" class="mb-1">T</label>
+            <input
+              type="checkbox"
+              value="thu"
+              id="thu"
+              v-model="trainingDays"
+            />
+          </div>
+          <div class="flex flex-col items-center m-3">
+            <label for="fri" class="mb-1">F</label>
+            <input
+              type="checkbox"
+              value="fri"
+              id="fri"
+              v-model="trainingDays"
+            />
+          </div>
+          <div class="flex flex-col items-center m-3">
+            <label for="sat" class="mb-1">S</label>
+            <input
+              type="checkbox"
+              value="sat"
+              id="sat"
+              v-model="trainingDays"
+            />
+          </div>
+          <div class="flex flex-col items-center m-3">
+            <label for="sun" class="mb-1">S</label>
+            <input
+              type="checkbox"
+              value="sun"
+              id="sun"
+              v-model="trainingDays"
+            />
+          </div>
+        </div>
+        <BaseButton
+          @click.prevent="storeValues"
+          type="submit"
+          class="mt-3 p-3 bg-blue-600"
+          >Start</BaseButton
+        >
       </form>
     </SwiperSlide>
   </Swiper>
@@ -227,6 +437,6 @@ SwiperCore.use([Pagination])
 
 <style>
 .swiper-pagination-bullet-active-main {
-  background-color: #dc2626;
+  background-color: #2563eb;
 }
 </style>
